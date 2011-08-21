f91e520960c17e80cfb7343e42ec4befc8de8415
Roman Rader <antigluk@gmail.com>
2011-08-21 11:59:28 +0300

    singleton

--- NotificationCenter.py
+++ NotificationCenter.py
@@ -34,7 +34,19 @@ Python implementation of Cocoa NSNotificationCenter
 
 """
 
+class Singleton(type):
+    def __init__(cls, name, bases, dic):
+        super(Singleton, cls).__init__(name, bases, dic)
+        cls.instance = None
+
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+        return cls.instance
+
 class NotificationCenter(object):
+
+    __metaclass__ = Singleton
 
     def __init__(self):
         self.notifications = {}
